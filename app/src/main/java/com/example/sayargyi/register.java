61459260be42b52ffb872563c823c32801879017
Zahra13s <122019063+Zahra13s@users.noreply.github.com>
2024-01-16 23:16:20 +0630

    3 interface done

--- app/src/main/java/com/example/sayargyi/register.java
+++ app/src/main/java/com/example/sayargyi/register.java
@@ -3,12 +3,14 @@ package com.example.sayargyi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
+
     }
 }
