bb505939042c551c6f1a3786139dd994fc498fe6
PyaePhyo229 <156043900+PyaePhyo229@users.noreply.github.com>
2024-01-30 10:00:56 +0630

    Delete Main

---
+++ app/src/main/java/com/example/sayargyi/AsiaFoodRowItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.sayargyi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AsiaFoodRowItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.asia_food_row_item);
+    }
+}
